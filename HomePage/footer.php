0ac8623bc2ec26626c773fcbc7227ca473d3ac47
thanhan002 <92962918+thanhan002@users.noreply.github.com>
2022-03-27 12:25:21 +0700

    STBS-25 Home Page

---
+++ HomePage/footer.php
@@ -0,0 +1,30 @@
+<footer id="footer" class="footer color-bg">
+    <div class="links-social inner-top-sm">
+        <footer class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+                                    <h3 style="color: #fff;">Shopping Portal</h3>
+                                    <li>
+                                        <a href="./admin/"><i class="icon fa fa-sign-in"></i>Adminn</a>
+                                    </li>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="module-body m-t-20">
+                            <p class="about-us" style="color: #fff;"> Brought To You By <a href="./">shopping portal </a></p>
+                            <div class="social-icons">
+                                <a href="" class='active'><i class="icon fa fa-facebook"></i></a>
+                                <a href="###"><i class="icon fa fa-github"></i></a>
+                                <a href="##"><i class="icon fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</footer>
